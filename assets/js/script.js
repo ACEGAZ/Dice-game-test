4055945af12ed1503e558b29c529da146464c461
ACEGAZ <garylowles@gmail.com>
2022-03-21 09:27:53 +0000

    made computer dice roll

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,22 @@ let playerScore = 0;
 let computerScore = 0;
 let moves = 0;
 
-function numberGenerator() {
+function playerNumberGenerator() {
+    return Math.floor(Math.random() * 6) + 1;
+ 
+}
+
+function computerNumberGenerator() {
     return Math.floor(Math.random() * 6) + 1;
  
 }
 
 function playerResult() {
-    document.getElementById('player-result').innerHTML = numberGenerator() 
+    document.getElementById('player-result').innerHTML = playerNumberGenerator() 
+}
+
+function computerResult() {
+    document.getElementById('computer-result').innerHTML = computerNumberGenerator() 
 }
 
 
