c67923b64a50057a7e38215286931bb97539df63
ACEGAZ <garylowles@gmail.com>
2022-03-23 19:34:10 +0000

    add restart button and styled layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,21 +9,30 @@ function playGame(){
    document.getElementById('player-result').innerHTML = playerRoll 
    document.getElementById('computer-result').innerHTML = computerRoll
    if (playerRoll > computerRoll) {
-       document.getElementById('result').innerHTML = 'You Win';
+       document.getElementById('result').innerHTML = 'Win';
        document.getElementById('player-score').innerHTML = playerScore++;
    } else if (playerRoll < computerRoll) { 
-    document.getElementById('result').innerHTML = 'You Lose';
+    document.getElementById('result').innerHTML = 'Lose';
     document.getElementById('computer-score').innerHTML = computerScore++;
    } else if (playerRoll === computerRoll) {
     document.getElementById('result').innerHTML = 'Draw';
    } if (playerScore === 11) {
-    document.getElementById('winner-loser').innerHTML = 'You Win The Game';
+    document.getElementById('winner-loser').innerHTML = 'You Win The Game!';
     document.getElementById('dice').remove();
    } if (computerScore === 11) {
-    document.getElementById('winner-loser').innerHTML = 'You Lose The Game';
+    document.getElementById('winner-loser').innerHTML = 'You Lose The Game!';
     document.getElementById('dice').remove();
    }
 }
+
+function restartGame() { 
+     window.location.reload()   
+    }
+
+    
+
+ 
+
 
 
 
