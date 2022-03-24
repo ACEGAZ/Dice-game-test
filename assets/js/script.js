4d16c3b0e3f78e2dfdc4986428ae1bfbc1c756b9
ACEGAZ <garylowles@gmail.com>
2022-03-24 10:03:19 +0000

    remove elemnts when win or lose

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 let playerScore = 1;
 let computerScore = 1;
-let x = 10;
+let removeElements = document.getElementsByClassName('end-game')
+
 
 
 function playGame(){
@@ -9,19 +10,29 @@ function playGame(){
    document.getElementById('player-result').innerHTML = playerRoll 
    document.getElementById('computer-result').innerHTML = computerRoll
    if (playerRoll > computerRoll) {
-       document.getElementById('result').innerHTML = 'Win';
+       document.getElementById('win-lose-draw').innerHTML = 'Win';
        document.getElementById('player-score').innerHTML = playerScore++;
    } else if (playerRoll < computerRoll) { 
-    document.getElementById('result').innerHTML = 'Lose';
+    document.getElementById('win-lose-draw').innerHTML = 'Lose';
     document.getElementById('computer-score').innerHTML = computerScore++;
    } else if (playerRoll === computerRoll) {
-    document.getElementById('result').innerHTML = 'Draw';
+    document.getElementById('win-lose-draw').innerHTML = 'Draw';
    } if (playerScore === 11) {
     document.getElementById('winner-loser').innerHTML = 'You Win The Game!';
     document.getElementById('dice').remove();
+    document.getElementById('player').remove();
+    document.getElementById('computer').remove();
+    document.getElementById('scores').remove();
+    document.getElementById('result-wrapper').remove();;
+    document.getElementById('win-lose-draw').remove();
    } if (computerScore === 11) {
     document.getElementById('winner-loser').innerHTML = 'You Lose The Game!';
     document.getElementById('dice').remove();
+    document.getElementById('player').remove();
+    document.getElementById('computer').remove();
+    document.getElementById('scores').remove();
+    document.getElementById('result-wrapper').remove();
+    document.getElementById('win-lose-draw').remove();
    }
 }
 
